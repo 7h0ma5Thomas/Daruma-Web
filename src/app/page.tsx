2b2a774fbc3a873ad4182a4eb53eebf727cdd8c2
Thomas <thomas.chaudru@gmail.com>
2023-11-23 18:32:13 +0100

    Header added + burger menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,40 @@
 import Header from '@/components/widgets/header'
 import React from 'react'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import { ChevronsDown } from 'lucide-react'
 
 export default function Home() {
   return (
-    <main className="w-full min-h-screen">
+    <main className="w-full min-h-screen flex relative">
       <Header />
+      <Image
+        className="brightness-50 h-screen w-full object-cover absolute"
+        src="/assets/home.jpg"
+        alt="Un ordinateur sur un bureau avec un smratphone et un bloc note"
+        width={6144}
+        height={4069}
+      />
+      <div className="absolute top-1/3 flex flex-col items-center px-2">
+        <h1
+          className="w-full text-center text-white text-5xl font-bold
+        mb-4"
+        >
+          Développeur web frontend
+        </h1>
+        <Button
+          className="w-32 bg-red-700 opacity-70 border-none font-bold text-white"
+          variant="outline"
+        >
+          Button
+        </Button>
+      </div>
+      <div className="absolute w-full top-[90%] flex flex-col items-center">
+        <div className="w-64 bg-[#00000038] flex justify-center">
+          <h2 className="text-white text-xl">Votre site web sur mesure</h2>
+        </div>
+        <ChevronsDown className="mt-6 animate-bounce" size={40} color="white" />
+      </div>
     </main>
   )
 }
--- src/components/widgets/header.tsx
+++ src/components/widgets/header.tsx
@@ -1,15 +1,90 @@
 import React from 'react'
 import Image from 'next/image'
+import { Home, Linkedin, Mail, Menu } from 'lucide-react'
+import Link from 'next/link'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTrigger,
+} from '@/components/ui/sheet'
 
 export default function Header() {
   return (
-    <header>
-      <Image
-        src="/assets/daruma-web.jpg"
-        alt="Logo Daruma-web"
-        width={472}
-        height={109}
-      />
+    <header className="w-full flex p-4 items-center justify-between fixed z-50">
+      <Link href="/">
+        <Image
+          className="w-56 rounded-xl"
+          src="/assets/daruma-web.jpg"
+          alt="Logo Daruma-web"
+          width={472}
+          height={109}
+        />
+      </Link>
+
+      <Sheet>
+        <SheetTrigger>
+          <Menu color="white" size={40} />
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetDescription>
+              <div className="flex justify-center">
+                <Image
+                  className="mt-6 "
+                  src="/assets/logoDaruma.png"
+                  alt="Logo Daruma-web"
+                  height={106}
+                  width={106}
+                />
+              </div>
+
+              <div className="flex flex-col mt-10 items-start">
+                <Link className="text-xl font-bold mt-4" href="/">
+                  {/* <Home size={32} strokeWidth={2.5} /> */}
+                  Accueil
+                </Link>
+                <Link className="text-xl font-bold mt-8" href="/">
+                  Services
+                </Link>
+                <Link className="text-xl font-bold mt-8" href="/">
+                  Qui suis-je
+                </Link>
+                <Link className="text-xl font-bold mt-8" href="/">
+                  Réalisations
+                </Link>
+                <Link className="text-xl font-bold mt-8" href="/">
+                  Compétences
+                </Link>
+                <Link className="text-xl font-bold mt-8" href="/">
+                  {/* <Mail size={32} strokeWidth={2.5} /> */}
+                  Contact
+                </Link>
+              </div>
+              <div className="flex items-center mt-20">
+                <Link href="/">
+                  <Image
+                    src="/assets/LinkedInMini.jpg"
+                    alt="Logo LinkedIn"
+                    height={40}
+                    width={40}
+                  />
+                </Link>
+                <Link className="ml-4" href="/">
+                  <Image
+                    className="border border-red-400 rounded-full"
+                    src="/assets/malt.png"
+                    alt="Logo Malt"
+                    height={40}
+                    width={40}
+                  />
+                </Link>
+              </div>
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
     </header>
   )
 }
